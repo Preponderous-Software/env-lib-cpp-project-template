8af0c46d9d2769ab5099802814c5967294dc6dba
dmccoystephenson <dmccoystephenson@gmail.com>
2022-09-11 16:36:46 -0600

    Removed assertions.

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,6 @@
 #include <unistd.h>
 #endif
 
-#include <assert.h>
-
 #include <string>
 #include <iostream>
 
@@ -27,31 +25,13 @@ std::vector<Entity> generateEntities(int numEntities) {
 
 void initiateEntityMovement(std::vector<Entity>& entities, Environment& environment) {
     for (Entity& entity : entities) {
-        Entity& retrievedEntity = environment.getEntity(entity.getId());
-        std::string initialLocationId = retrievedEntity.getLocationId();
-        assert(initialLocationId != "N/S");
-        
-        bool success = environment.moveEntityToRandomAdjacentLocation(retrievedEntity.getId());
-        std::string currentLocationId = retrievedEntity.getLocationId();
-
-        if (success) {
-            // assert that the entity's location is adjacent to the previous location
-            Location& initialLocation = environment.getGrid()->getLocation(initialLocationId);
-            Location& currentLocation = environment.getGrid()->getLocation(currentLocationId);
-            assert((initialLocation.getX() == currentLocation.getX() && initialLocation.getY() - 1 == currentLocation.getY()) ||
-                    (initialLocation.getX() == currentLocation.getX() + 1 && initialLocation.getY() == currentLocation.getY()) ||
-                    (initialLocation.getX() == currentLocation.getX() && initialLocation.getY() + 1 == currentLocation.getY()) ||
-                    (initialLocation.getX() == currentLocation.getX() - 1 && initialLocation.getY() == currentLocation.getY()));
-        }
-        else {
-            // assert that the entity's location has not changed
-            assert(currentLocationId == initialLocationId);
-        }
+        Entity& retrievedEntity = environment.getEntity(entity.getId());     
+        environment.moveEntityToRandomAdjacentLocation(retrievedEntity.getId());
     }
 }
 
 int main() {
-    int environmentSize = 8;
+    int environmentSize = 16;
     Environment environment(0, "Earth", environmentSize);
     std::vector<Entity> entities = generateEntities(environmentSize);
     for (Entity& entity : entities) {
